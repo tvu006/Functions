412ccca6e13ec5525bb06341bb66723d2b51d7dd
Tony Vu <38059374+tvu006@users.noreply.github.com>
2018-12-15 15:19:06 -0500

    FactorFinder
    Prints all of the factors of a given decimal number

---
+++ FactorFinder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/*
+ * Tony Vu
+ * Prints all of the factors of a given decimal number
+ */
+public class FactorFinder {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		factors(input.nextInt());
+	}
+	
+	public static void factors(int number) {
+		if (number < 1) System.out.print("Invalid input.");
+		for (int i = 1; i <= number; i++) {
+			if (number % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
