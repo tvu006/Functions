5c5b7eed85fc91542c3212ab71911f10772e611e
Tony Vu <38059374+tvu006@users.noreply.github.com>
2018-12-15 16:07:56 -0500

    Update PrimeFinder.java

--- PrimeFinder.java
+++ PrimeFinder.java
@@ -2,20 +2,27 @@ import java.util.*;
 
 /*
  * Tony Vu
- * 15/12/2018
+ * Created: 15/12/2018
  * Prints all prime numbers starting and ending from given points
  */
 public class PrimeFinder {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
 		System.out.print("Enter the first number: ");
 		int first = input.nextInt();
 		System.out.print("Enter the last number: ");
 		int last = input.nextInt();
-		primeOfInterval(first, last);
+		primes = primeOfInterval(first, last);
+		System.out.println("Primes from " + first + " to " + last + " :" + primes.toString());
+		System.out.println("Sum of primes: " + sum(primes));
+		System.out.println("Product of primes: " + product(primes));
 	}
 	
-	public static void primeOfInterval(int first, int last) {
+	public static ArrayList<Integer> primeOfInterval(int first, int last) {
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
 		if (last <= first || first <= 1) System.out.println("Invalid input");
 		for (int i = first; i < last; i++) {
 			boolean prime = true;
@@ -27,11 +34,29 @@ public class PrimeFinder {
 					}
 				}
 				if (prime) {
-					System.out.print(i + " ");
+					primes.add(i);
 				} else {
 					prime = true;
 				}
 			}
 		}
+		
+		return primes;
+	}
+	
+	public static int sum(ArrayList<Integer> list) {
+		int sum = 0;
+		for (int i: list) {
+			sum += i;
+		}
+		return sum;
+	}
+	
+	public static int product(ArrayList<Integer> list) {
+		int product = 1;
+		for (int i: list) {
+			product *= i;
+		}
+		return product;
 	}
 }
